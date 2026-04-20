f1f93397fb41ba7280ecba22867ae4258fa4b53a
ibyeonggyu <ibyeonggyu410@gmail.com>
2026-04-20 17:09:19 +0900

    Update account_manager.h

--- account_manager.h
+++ account_manager.h
@@ -5,26 +5,28 @@
 #include <QList>
 #include <QString>
 
+
+// 계좌 목록 정리
 class AccountManager {
 public:
     AccountManager();
-
-    QList<Account>& getAccounts();
-    Account& getCurrentAccount();
-    void setCurrentIndex(int index);
-    int getCurrentIndex() const;
-
-    bool deposit(int amount);
-    bool withdraw(int amount);
+    
+    //계좌 목록 관련
+    QList<Account>& getAccounts();              // 전체 계좌 목록
+    Account& getCurrentAccount();               // 현재 계좌 목록
+    void setCurrentIndex(int index);            // 현재 계좌 변경(더블 클릭하면 호출하는 구조)
+    int getCurrentIndex() const;                // 현재 계좌 이름, 번호 반환
+    
+    //거래 관련
+    bool deposit(int amount);                   //입금
+    bool withdraw(int amount);                  //출금
     bool transfer(int amount, QString targetBank, bool isMyAccount, QString fromBank);
-
-    int getTotalBalance() const;
-
-    // ↓ 삭제! Account 클래스 안으로 들어감
-    // void addTransaction(QString type, int amount, QString target);
+    // 송금
+    
+    int getTotalBalance() const;                // 전체 계좌 잔액 합산
 
 private:
-    QList<Account> accounts;
+    QList<Account> accounts; 
     int currentIndex;
     void loadDummyData();
 };
